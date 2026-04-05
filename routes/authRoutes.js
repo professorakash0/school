854f27fd0f2fa445252ff81f5d1e3fc4ec5a2e0c
professorakash0 <professorakash0@gmail.com>
2026-04-05 09:30:54 -0700

    removing register controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { registerAdmin, loginAdmin } from '../controllers/authController.js';
+import { loginAdmin } from '../controllers/authController.js';
 
 const router = express.Router();
 
-
-router.post('/register', registerAdmin);
 router.post('/login', loginAdmin);
 
 export default router;
