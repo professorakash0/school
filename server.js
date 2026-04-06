3867595f54b88ae904905200024a8a505d362a7f
professorakash0 <professorakash0@gmail.com>
2026-04-06 11:40:21 -0700

    optimizing backend +

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.get('/', (req, res) => {
   res.send('School Management System API is running...');
 });
 
+//port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
